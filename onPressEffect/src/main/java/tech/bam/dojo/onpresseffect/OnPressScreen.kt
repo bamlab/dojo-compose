da8ca9f7540598deeaffc435d420ceea9e24e1ed
Maxence Leroy <maxencel@bam.tech>
2022-10-12 12:53:58 +0200

    End of dojo

--- onPressEffect/src/main/java/tech/bam/dojo/onpresseffect/OnPressScreen.kt
+++ onPressEffect/src/main/java/tech/bam/dojo/onpresseffect/OnPressScreen.kt
@@ -1,13 +1,23 @@
 package tech.bam.dojo.onpresseffect
 
+import android.util.Log
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import java.lang.Float
 
 @Composable
 fun OnPressScreen() {
@@ -15,10 +25,34 @@ fun OnPressScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        val componentSize = remember { mutableStateOf(IntSize(0, 0)) }
+        val tapInput: MutableState<Offset> = remember { mutableStateOf(Offset(0f, 0f)) }
+
+        val animatedTap by animateOffsetAsState(tapInput.value)
+
         FidgetCard(
             Modifier
                 .fillMaxWidth(0.66f)
                 .fillMaxHeight(0.2f)
+                .onGloballyPositioned { layoutCoordinates ->
+                    componentSize.value = layoutCoordinates.size
+                }
+                .pointerInput(null) {
+                    this.detectTapGestures { offset ->
+                        tapInput.value = offset
+                    }
+                }
+                .graphicsLayer {
+                    val height = componentSize.value.height.toFloat()
+                    val width = componentSize.value.width.toFloat()
+                    val transformation = calcTransformationFromInput(
+                        animatedTap,
+                        width,
+                        height
+                    )
+                    this.rotationX = transformation.first
+                    this.rotationY = transformation.second
+                }
         )
     }
 }
