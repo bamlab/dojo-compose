0a52a027185331df239a82eaeeb125c5e22b05d3
Dennis BORDET <dennisb@bam.tech>
2024-08-21 14:53:58 +0200

    refactor: new method to add a dojo

---
+++ composeApp/src/commonMain/kotlin/tech/bam/dojo/compose/HomeScreen.kt
@@ -0,0 +1,41 @@
+package tech.bam.dojo.compose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+
+class HomeScreen : Screen {
+    @Composable
+    override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+        Column(
+            modifier = Modifier.fillMaxSize().padding(32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement =
+                Arrangement.spacedBy(
+                    16.dp,
+                    Alignment.CenterVertically,
+                ),
+        ) {
+            val greeting = remember { Greeting().greet() }
+            Text(greeting, style = MaterialTheme.typography.h6)
+            Spacer(modifier = Modifier.padding(16.dp))
+
+            for (screen in screenList) {
+                NavButton(screen, navigator)
+            }
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/tech/bam/dojo/compose/screenList.kt
@@ -0,0 +1,8 @@
+package tech.bam.dojo.compose
+
+import tech.bam.dojo.onpresseffect.OnPressScreen
+
+val screenList =
+    listOf(
+        OnPressScreen(),
+    )
