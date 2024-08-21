0ca393bcf74144ea5ba7f08e665cba924a69dcb3
Dennis BORDET <dennisb@bam.tech>
2024-08-21 11:59:22 +0200

    feat: add previews

---
+++ onPressEffect/src/androidMain/kotlin/Previews.kt
@@ -0,0 +1,7 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import tech.bam.dojo.onpresseffect.OnPressScreen
+
+@Preview
+@Composable
+fun OnPressScreenPreview() = OnPressScreen()
