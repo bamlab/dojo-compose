607f00f8af7306db76994547d86990909da7896a
Paul <paulb@bam.tech>
2024-08-30 14:47:15 +0200

    first session

--- FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/PremiumButton.kt
+++ FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/PremiumButton.kt
@@ -1,22 +1,45 @@
 package tech.bam.dojo.freetimelancetracker.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonColors
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import tech.bam.dojo.freetimelancetracker.theme.FreetimelanceTrackerColors
 
 @Composable
 fun PremiumButton(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier.background(FreetimelanceTrackerColors.white),
-        contentAlignment = Alignment.Center,
-    ) {
-        Text(
-            text = "Premium Fake Button",
-            color = FreetimelanceTrackerColors.purpleDark,
-        )
-    }
+        Button(
+            onClick = { },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = FreetimelanceTrackerColors.yellow
+            ),
+            shape = RoundedCornerShape(
+                size = 16.dp
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(vertical = 20.dp),
+            border = BorderStroke(
+                1.dp,
+                Color.White
+            )
+        ) {
+            Text(
+                text = "\uD83D\uDD25 Passer premium \uD83D\uDD25 ",
+                color = FreetimelanceTrackerColors.purpleDark,
+            )
+        }
 }
--- SimpleLayout/src/commonMain/kotlin/tech/bam/dojo/simplelayout/SimpleLayoutScreen.kt
+++ SimpleLayout/src/commonMain/kotlin/tech/bam/dojo/simplelayout/SimpleLayoutScreen.kt
@@ -1,14 +1,25 @@
 package tech.bam.dojo.simplelayout
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import tech.bam.dojo.freetimelancetracker.components.AvatarCard
+import tech.bam.dojo.freetimelancetracker.components.ContactCard
+import tech.bam.dojo.freetimelancetracker.components.PremiumButton
+import tech.bam.dojo.freetimelancetracker.theme.FreetimelanceTrackerColors
 
 class SimpleLayoutScreen : Screen {
     override val key = "Simple Layout"
@@ -21,10 +32,18 @@ class SimpleLayoutScreen : Screen {
 
 @Composable
 fun SimpleLayoutView() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(FreetimelanceTrackerColors.purple)
+            .padding(
+                horizontal = 16.dp
+            )
     ) {
         AvatarCard()
+        ContactCard()
+        PremiumButton()
     }
 }
