4e1aed5bb85a594eacc8fc314f15033753359ee7
Dennis BORDET <dennisb@bam.tech>
2024-09-05 12:35:10 +0200

    feat: border

--- FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/PremiumButton.kt
+++ FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/PremiumButton.kt
@@ -1,6 +1,7 @@
 package tech.bam.dojo.freetimelancetracker.components
 
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
@@ -9,24 +10,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import tech.bam.dojo.freetimelancetracker.text.FreetimelanceTrackerStrings
 import tech.bam.dojo.freetimelancetracker.theme.FreetimelanceTrackerColors
+import tech.bam.dojo.freetimelancetracker.theme.FreetimelanceTrackerDimens
 
 @Composable
 fun PremiumButton(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit,
+    onClick: () -> Unit
 ) {
     Button(
         modifier = modifier,
-        shape = RoundedCornerShape(16.dp),
+        shape = FreetimelanceTrackerDimens.roundedShape,
         onClick = onClick,
-        colors =
-            ButtonDefaults.buttonColors(
-                backgroundColor = FreetimelanceTrackerColors.yellow,
-                contentColor = FreetimelanceTrackerColors.purpleDark,
-            ),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = FreetimelanceTrackerColors.yellow,
+            contentColor = FreetimelanceTrackerColors.purpleDark
+        ),
+        border = BorderStroke(1.dp, FreetimelanceTrackerColors.white)
     ) {
         Text(
-            text = FreetimelanceTrackerStrings.premium_button_label,
+            text = FreetimelanceTrackerStrings.premium_button_label
         )
     }
 }
