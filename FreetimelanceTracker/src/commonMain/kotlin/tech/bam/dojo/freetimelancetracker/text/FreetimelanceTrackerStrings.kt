c937b73707f45137c7a580408f1ce6f7c96501e9
Dennis BORDET <dennisb@bam.tech>
2024-09-05 12:07:04 +0200

    feat: prepare for next session

---
+++ FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/text/FreetimelanceTrackerStrings.kt
@@ -0,0 +1,18 @@
+package tech.bam.dojo.freetimelancetracker.text
+
+// TODO use resources
+// As resources do not work with compose preview until version 1.7.0 on android studio but it's OK with fleet
+object FreetimelanceTrackerStrings {
+    const val avatar_name_fake_value = "Marguerite Duras"
+    const val avatar_title_fake_value = "Product Designer"
+    const val avatar_age_fake_value = "30 ans"
+    const val contact_label = "Contact"
+    const val contact_website_label = "Site Web"
+    const val contact_website_fake_value = "www.mduras.com"
+    const val contact_email_label = "Email"
+    const val contact_email_fake_value = "mduras@gmail.com"
+    const val contact_phone_label = "Téléphone"
+    const val contact_phone_fake_value = "06.01.23.45.67"
+    const val contact_membership_duration_fake_value = "Membre depuis mars 2022"
+    const val premium_button_label = "🔥 Passer Premium 🔥"
+}
