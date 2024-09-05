5514f1bcf21c6a401dff4bce06caf2aad7c192f4
Dennis BORDET <dennisb@bam.tech>
2024-09-05 12:35:10 +0200

    feat: border

--- FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/theme/FreetimelanceTrackerDimens.kt
+++ FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/theme/FreetimelanceTrackerDimens.kt
@@ -1,5 +1,6 @@
 package tech.bam.dojo.freetimelancetracker.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.unit.dp
 
 object FreetimelanceTrackerDimens {
@@ -8,4 +9,5 @@ object FreetimelanceTrackerDimens {
     val backgroundBlur = 15.dp
     val cardBlur = 15.dp
     val iconSize = 32.dp
+    val roundedShape = RoundedCornerShape(16.dp)
 }
