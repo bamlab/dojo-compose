9e73b8cc65b1ea51c43942d3288662cf9484458d
Dennis BORDET <dennisb@bam.tech>
2024-09-05 12:07:04 +0200

    feat: prepare for next session

--- FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/AvatarCard.kt
+++ FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/AvatarCard.kt
@@ -6,6 +6,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import dojocompose.freetimelancetracker.generated.resources.avatar_age_fake_value
+import dojocompose.freetimelancetracker.generated.resources.avatar_name_fake_value
+import dojocompose.freetimelancetracker.generated.resources.avatar_title_fake_value
+import tech.bam.dojo.freetimelancetracker.text.FreetimelanceTrackerStrings
 import tech.bam.dojo.freetimelancetracker.theme.FreetimelanceTrackerColors
 
 @Composable
@@ -14,9 +18,8 @@ fun AvatarCard(modifier: Modifier = Modifier) {
         modifier = modifier.background(FreetimelanceTrackerColors.white),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            text = "Avatar Fake Card",
-            color = FreetimelanceTrackerColors.purpleDark,
-        )
+        Text(text = FreetimelanceTrackerStrings.avatar_name_fake_value)
+        Text(text = FreetimelanceTrackerStrings.avatar_title_fake_value)
+        Text(text = FreetimelanceTrackerStrings.avatar_age_fake_value)
     }
 }
