e3ed04c994f7917887d6881e8685232ce8e89271
Dennis BORDET <dennisb@bam.tech>
2024-09-13 14:23:52 +0200

    feat: manage premiumButton Click

--- FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/PremiumButton.kt
+++ FreetimelanceTracker/src/commonMain/kotlin/tech/bam/dojo/freetimelancetracker/components/PremiumButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import tech.bam.dojo.freetimelancetracker.text.FreetimelanceTrackerStrings
@@ -14,7 +15,8 @@ import tech.bam.dojo.freetimelancetracker.theme.FreetimelanceTrackerDimens
 @Composable
 fun PremiumButton(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    isPremium: Boolean
 ) {
     Button(
         modifier = modifier,
@@ -24,10 +26,18 @@ fun PremiumButton(
             backgroundColor = FreetimelanceTrackerColors.yellow,
             contentColor = FreetimelanceTrackerColors.purpleDark
         ),
-        border = BorderStroke(1.dp, FreetimelanceTrackerColors.white)
+        border = BorderStroke(1.dp, FreetimelanceTrackerColors.white),
+        enabled = !isPremium
     ) {
+        val buttonText = remember(isPremium) {
+            if (isPremium) {
+                FreetimelanceTrackerStrings.premium_Repository_button_label_premium
+            } else {
+                FreetimelanceTrackerStrings.premium_Repository_button_label
+            }
+        }
         Text(
-            text = FreetimelanceTrackerStrings.premium_Repository_button_label
+            text = buttonText
         )
     }
 }
--- SimpleLayout/src/commonMain/kotlin/tech/bam/dojo/simplelayout/SimpleLayoutViewModel.kt
+++ SimpleLayout/src/commonMain/kotlin/tech/bam/dojo/simplelayout/SimpleLayoutViewModel.kt
@@ -24,5 +24,6 @@ class SimpleLayoutViewModel(
     )
 
     fun onPremiumButtonClick() {
+        repository.setPremium()
     }
 }
